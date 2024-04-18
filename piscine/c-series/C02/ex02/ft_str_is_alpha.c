0709927b01c9769523afd7e3e512fca3f0c42cc9
janghw <elonj33@gmail.com>
2024-04-18 21:53:20 +0200

    s

--- piscine/c-series/C02/ex02/ft_str_is_alpha.c
+++ piscine/c-series/C02/ex02/ft_str_is_alpha.c
@@ -6,16 +6,15 @@
 /*   By: jhyeongw <jhyeongw@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 21:21:46 by jhyeongw          #+#    #+#             */
-/*   Updated: 2024/03/28 19:17:50 by jhyeongw         ###   ########.fr       */
+/*   Updated: 2024/04/17 19:52:49 by jhyeongw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // created by JangHW on 2024-03-20 20:29
 int     ft_str_is_alpha(char *str);
 
-/*
 #include <stdio.h>
-int main()
+int      main()
 {
     char a[] = "aSAASa";
     char b[] = "aBa4dfe";
@@ -25,13 +24,12 @@ int main()
     printf("%d", ft_str_is_alpha(c));
 
 }
-*/
 
 int     ft_str_is_alpha(char *str)
 {
     int i;
     i = 0;
-    if(str[0] == '\0')
+    if (str[0] == '\0')
         return 1;
 
     while(str[i])
