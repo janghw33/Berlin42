0378aa9e1e941fd4651daefbd356422505b82cb1
janghw <elonj33@gmail.com>
2024-04-21 18:13:18 +0200

    first try

---
+++ piscine-reload/ex06/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhyeongw <jhyeongw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 00:03:11 by jhyeongw          #+#    #+#             */
+/*   Updated: 2024/04/20 00:23:49 by jhyeongw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+
+// void	ft_print_alphabet(void);
+
+// void	ft_putchar(char a)
+// {
+// 	write(1, &a, 1);
+// }
+
+// int	main(void)
+// {
+// 	ft_print_alphabet();
+// }
+
+void	ft_print_alphabet(void)
+{
+	unsigned char	a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		ft_putchar(a);
+		a++;
+	}
+}
---
+++ piscine-reload/ex07/ft_print_numbers.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhyeongw <jhyeongw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 00:14:36 by jhyeongw          #+#    #+#             */
+/*   Updated: 2024/04/20 00:31:41 by jhyeongw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <unistd.h>
+
+// void	ft_print_numbers(void);
+
+// void	ft_putchar(char c)
+// {
+// 	write(1, &c, 1);
+// }
+
+// int	main(void)
+// {
+// 	ft_print_numbers();
+// }
+
+void	ft_print_numbers(void)
+{
+	unsigned char	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
